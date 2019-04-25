bef3ccab6c48d02d9770be9bec0eda6ec35ed81b
Victor Taksheyev <victortaksheyev@gmail.com>
2019-04-24 20:21:27 -0700

    Update SevenSeg.ino

--- SevenSeg/SevenSeg.ino
+++ SevenSeg/SevenSeg.ino
@@ -2,6 +2,8 @@
 
 Servo servo; 
 
+const int NUM_SERVOS = 2; // change to 7
+
 int number = 0;
 
 int BCDToSS[16][7] = {
@@ -24,11 +26,21 @@ int BCDToSS[16][7] = {
 };
 
 void setup() {
-  servo.attach(0);
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    s[i].attach(i); // initialize pins
+    s[i].write(0);  // initialize angle
+  }
 }
 
 void loop() {
   servo.write((BCDToSS[number%16][0] * 80) + 10);
   delay(1000);
   number++;
+  
+  for (int i = 0; i < NUM_SERVOS; i++)
+      s[i].write(90);
+  delay(2000);
+  for (int i = 0; i < NUM_SERVOS; i++)
+    s[i].write(0);
+  delay(2000);
 }
